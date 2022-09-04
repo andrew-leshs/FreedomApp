994fcb53771b79e011367b9c6862c4bd1445fa5a
Your Name <you@example.com>
2022-09-04 20:48:05 +0300

    f

--- app/src/main/java/ru/freedomapps/ListActivity.java
+++ app/src/main/java/ru/freedomapps/ListActivity.java
@@ -1,8 +1,21 @@
 package ru.freedomapps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import ru.freedomapps.fragments.FirstFragment;
+import ru.freedomapps.fragments.ForthFragment;
+import ru.freedomapps.fragments.SecondFragment;
+import ru.freedomapps.fragments.ThirdFragment;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -10,5 +23,37 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        BottomNavigationView bottomNavigationView;
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        openFragment(FirstFragment.newInstance("",""));
+                        return true;
+                    case R.id.navigation_explore:
+                        openFragment(SecondFragment.newInstance("",""));
+                        return true;
+                    case R.id.navigation_subscriptions:
+                        openFragment(ThirdFragment.newInstance("",""));
+                        return true;
+                    case R.id.navigation_library:
+                        openFragment(ForthFragment.newInstance("", ""));
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null); //if you add fragments it will be added to the backStack. If you replace the fragment it will add only the last fragment
+        transaction.commit(); // commit() performs the action
     }
 }
---
+++ app/src/main/java/ru/freedomapps/fragments/FirstFragment.java
@@ -0,0 +1,73 @@
+package ru.freedomapps.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+import ru.freedomapps.R;
+
+public class FirstFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ForthFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ru.freedomapps.fragments.FirstFragment newInstance(String param1, String param2) {
+        ru.freedomapps.fragments.FirstFragment fragment = new ru.freedomapps.fragments.FirstFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_first, container, false);
+        ImageView imageView = v.findViewById(R.id.addFile);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("application/apk");
+                startActivity(intent);
+            }
+        });
+
+        return v;
+    }
+}
